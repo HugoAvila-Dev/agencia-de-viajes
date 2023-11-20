d659f9bf9d3a2be31aeff5e5bccd9f69f14440f2
Hugo <hhugo.avila97@gmail.com>
2023-11-20 10:45:35 -0300

    feat: ✨ Hojas de estilo y archivos estaticos

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const port = process.env.PORT || 4000;
 //Habilitar PUG
 app.set('view engine', 'pug');
 
+//Definir la carpeta publica
+app.use(express.static('public'));
+
 //Agregar router 
-app.use('/', router)
+app.use('/', router);
 
 app.listen(port, () => {
    console.log(`El Servidor está funcionando en el puerto ${port}`)
