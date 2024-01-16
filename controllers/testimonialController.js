5a975407a7f8fc4a50e7e4db304f53ced2e783de
Hugo <hhugo.avila97@gmail.com>
2024-01-15 23:34:36 -0300

    feat: ✨ Validación de formularios

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -16,7 +16,20 @@ const guardarTestimonial = (req, res) => {
     if(mensaje.trim() === '') {
         errores.push({mensaje: 'El Mensaje está vacio'});
     }
-    console.log(errores)
+    if(errores.length > 0) {
+
+        //Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        })
+    } else {
+        // Almacenarlo en la base de datos
+        
+    }
 }
 
 export {
