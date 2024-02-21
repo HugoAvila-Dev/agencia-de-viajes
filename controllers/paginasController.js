a6bfddeb6af9a7c0cd01b6b38bc07b11bb3b4a10
Hugo <hhugo.avila97@gmail.com>
2024-02-21 16:11:17 -0300

    feat: ✨ trayendo los testimoniales desde la base de datos

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,4 +1,5 @@
 import { Viaje } from '../models/Viaje.js';
+import { Testimonial } from '../models/Testimoniales.js';
 
 const paginaInicio = (req, res) => { //req - lo que enviamos : res - lo que express nos responde
     res.render('inicio', {
@@ -22,10 +23,19 @@ const paginaViajes = async (req, res) => { //req - lo que enviamos : res - lo qu
     });
 }
 
-const paginaTestimoniales = (req, res) => { //req - lo que enviamos : res - lo que express nos responde
-    res.render('testimoniales', {
-        pagina: 'Testimoniales'
-    });
+const paginaTestimoniales = async (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    
+    try {
+        const testimoniales = await Testimonial.findAll()
+        
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            testimoniales
+        });
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
 //Muestra un viaje por su slug
