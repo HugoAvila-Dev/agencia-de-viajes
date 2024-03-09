d53baf10e9be35728c3c68ad5daa916bcc948688
Hugo <hhugo.avila97@gmail.com>
2024-03-09 11:41:38 -0300

    feat: ✨ Variables de Entorno

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import router from './routes/index.js';
 import db from './config/db.js';
+dotenv.config()
+
+console.log(process.env.DATABASE)
 
 const app = express();
 
